65aa4b4fad8798cc58a95568d51d2efe8389c577
Gabo-Mac <gabgonlo05@gmail.com>
2024-06-11 22:35:10 -0600

    FIX: insertar, FEAT: equilibrado, altura y calcularFE, el programa ya es funcional

--- avl.cpp
+++ avl.cpp
@@ -187,12 +187,21 @@ void profundidadNodo(nodoA *nodo, int &h) {
 		h = profN;
 }
 
-void alturaArbol(nodoA *raiz, int &h) {
-	if (raiz != NULL) {
-		alturaArbol(raiz->izq, h);
-		profundidadNodo(raiz, h);
-		alturaArbol(raiz->der, h);
-	}
+int alturaArbol(nodoA *raiz) {
+	if (raiz == NULL)
+		return 0;
+
+	int izqAltura = alturaArbol(raiz->izq);
+	int derAltura = alturaArbol(raiz->der);
+	
+	int mayor;
+	if (izqAltura > derAltura)
+		mayor = izqAltura;
+	else
+		mayor = derAltura;
+
+	return mayor + 1;
+	
 }
 
 int buscarDato(nodoA *raiz, int dato, nodoA* &actual, nodoA* &anterior) {
@@ -225,52 +234,212 @@ nodoA *nodoNuevo(nodoA* &padre, int datoN) {
 	return nuevo;
 }
 
-void equilibrar(nodoA *nodo, int rama, bool nuevo) {
+void equilibrar(nodoA* &raiz, nodoA *ultNodo, int rama, bool nuevo) {
+	bool salir = 0;
 
-}
-
-void insertarDato(nodoA* &raiz, int dato) {
-	nodoA *padreAux = NULL;
-	nodoA *actual = raiz;
-	int valAct = actual->valor;
-
-	while (actual != NULL && actual->valor != dato) {
-		padreAux = actual;
-		valAct = actual->valor;
-
-		if (dato > valAct)
-			actual = actual->der;
-		else
-			actual = actual->izq;
-	}
-
-	if (actual != NULL)
-		cout << "No se agregó, se repite el dato \n";
-	else {
-		if (padreAux == NULL) {
-			nodoA *nuevo = new nodoA;
-			nuevo->valor = dato;
-			raiz = nuevo;
+	while (ultNodo && !salir) {
+		if (nuevo) {
+			if (rama == izquierdo)
+				ultNodo->fe -= 1;
+			else
+				ultNodo->fe += 1;
 		}
 		else {
-			if (dato > padreAux->valor) {
-				padreAux->der = nodoNuevo(padreAux, dato);
-				equilibrar(padreAux, derecho, 1);
+			if (rama == izquierdo)
+				ultNodo->fe += 1;
+			else
+				ultNodo->fe -= 1;
+		}
+
+		if (ultNodo->fe == 0)
+			salir = 1;
+		else {
+			if (ultNodo->fe == -2) {
+				if (ultNodo->izq->fe == 1)
+					rotaDD(raiz, ultNodo);
+				else
+					rotaSD(raiz, ultNodo);
+
+				salir = 1;
 			}
+
 			else {
-				padreAux->der = nodoNuevo(padreAux, dato);
-				equilibrar(padreAux, izquierdo, 1);
+				if (ultNodo->fe == 2) {
+					if (ultNodo->der->fe == -1)
+						rotaDI(raiz, ultNodo);
+					else
+						rotaSI(raiz, ultNodo);
+					salir = 1;
+				}
+			}
+		}
+
+		if (!salir) {
+			nodoA *pad = ultNodo->padre;
+			if (pad) {
+				if (pad->izq == ultNodo)
+					rama = izquierdo;
+				else
+					rama = derecho;
+			}
+			ultNodo = pad;
+		}
+	}
+}
+
+void insertarDato(nodoA *&raiz, int elemento){
+	nodoA *nuevo;
+	nodoA *actual = raiz;
+	nodoA *anterior = NULL;
+
+	if(buscarDato(raiz, elemento, actual, anterior))
+		cout<<"Elemento duplicado\n";
+	else{
+		nuevo = new nodoA;
+		nuevo->valor = elemento;
+		nuevo->izq = NULL;
+		nuevo->der = NULL;
+		nuevo->padre = NULL;
+		nuevo->fe = 0;
+
+		if(anterior == NULL)
+			raiz = nuevo;
+		else{
+			nuevo->padre = anterior;
+			if(anterior->valor > elemento){
+				anterior->izq = nuevo;
+				equilibrar(raiz, anterior, izquierdo, 1);
+			}
+			else{
+				anterior->der = nuevo;
+				equilibrar(raiz, anterior, derecho, 1);
 			}
 		}
 	}
 }
 
-void eliminar(nodoA* &raiz, int dato) {
+bool esHoja(nodoA *nod) {
+	bool hoja;
+	if (nod->izq == NULL && nod->der == NULL)
+		hoja = 1;
+	else
+		hoja = 0;
 
+	return hoja;
+}
+
+int calcularFE(nodoA *nod) {
+	int fe;
+	if (nod == NULL)
+		fe = 0;
+	else
+		fe = alturaArbol(nod->der) - alturaArbol(nod->izq);
+
+	return fe;
+}
+
+void eliminar(nodoA *&raiz, int dato) {
+    nodoA *actual = raiz;
+    nodoA *padreAux = NULL;
+    nodoA *descendiente = NULL;
+    nodoA *ultimoNodo = NULL;
+
+    bool termina = 0;
+
+    while (!termina && actual != NULL) {
+        if (dato == actual->valor) {
+            ultimoNodo = actual->padre;
+
+            if (esHoja(actual)) {
+                if (padreAux != NULL) {
+                    if (padreAux->der == actual)
+                        padreAux->der = NULL;
+                    else if (padreAux->izq == actual)
+                        padreAux->izq = NULL;
+                }
+
+                delete actual;
+                actual = NULL;
+                termina = 1;
+            }
+            else {
+                padreAux = actual;
+                if (actual->der == NULL) {
+                    descendiente = actual->izq;
+                    while (descendiente->izq != NULL) {
+                        padreAux = descendiente;
+                        descendiente = descendiente->izq;
+                    }
+                }
+                else {
+                    descendiente = actual->der;
+                    while (descendiente->der != NULL) {
+                        padreAux = descendiente;
+                        descendiente = descendiente->der;
+                    }
+                }
+
+                int aux = actual->valor;
+                actual->valor = descendiente->valor;
+                descendiente->valor = aux;
+                actual = descendiente;
+            }
+        }
+        else {
+            padreAux = actual;
+            if (dato > actual->valor)
+                actual = actual->der;
+            else if (dato < actual->valor)
+                actual = actual->izq;
+        }
+    }
+
+    if (ultimoNodo) {
+    	int derizq;
+    	if (ultimoNodo->izq == actual)
+    		derizq = izquierdo;
+    	else
+    		derizq = derecho;
+
+    	equilibrar(raiz, ultimoNodo, derizq, 0);
+    }
+}
+
+void imprimirArbol(nodoA *arbol, int aux) {
+	if (arbol != NULL) {
+		imprimirArbol(arbol->der, aux + 1);
+		for (int i = 0; i < aux; i++)
+			cout << "\t";
+		cout << arbol->valor << "\n";
+		imprimirArbol(arbol->izq, aux + 1);
+	}
 }
 
 int main() {
+	nodoA *raiz = NULL;
+
 	cout << "Construcción de Árboles AVL \n";
+
+	bool band = 1;
+	while (band) {
+		int datoL;
+		cout << "Ingresar dato ";
+		cin >> datoL;
+
+		if (datoL > -1)
+			insertarDato(raiz, datoL);
+		else
+			band = 0;
+	}
+
+	imprimirArbol(raiz, 0);
+
+	int datoE;
+	cout << "Ingresar el dato a eliminar ";
+	cin >> datoE;
+	eliminar(raiz, datoE);
+
+	imprimirArbol(raiz, 0);
 
 	return 0;
 }
